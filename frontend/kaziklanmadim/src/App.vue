2fd95795df3de3135575ad2a2e1049260fee50d1
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2021-01-16 01:18:56 +0300

    Added frontend code

---
+++ frontend/kaziklanmadim/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Kazik" src="./assets/kazik1.jpg" />
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 20px;
+}
+</style>
---
+++ frontend/kaziklanmadim/src/components/Main.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">Kazıklanmadım :)</h1>
+      <hr class="my-4" />
+
+      <p class="lead">
+        Aldığın ürünün ismini yaz ve en ucuz fiyatı gör :) (eti cicibebe).
+      </p>
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        v-model="searchVariable"
+      />
+      <br />
+      <button class="btn btn-primary" type="submit">Search</button>
+    </div>
+
+    <br />
+
+    <div v-for="(data, index) in fakeData" :key="index">
+      {{ index }} {{ data }} TL <br />
+      <br />
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Main",
+
+  data() {
+    return {
+      searchVariable: "",
+
+      fakeData: {
+        "Cicibebe 100 gr": "Migros17",
+        "Cicibebe 200 gr": "Migros25",
+      },
+    };
+  },
+
+  methods: {
+    search() {
+      let url = "https://example.com/" + this.searchVariable;
+      axios
+        .get(url)
+        .then((response) => {
+          this.result = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
